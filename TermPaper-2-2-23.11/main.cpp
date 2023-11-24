a757fee79a6e5dc2613b74788875e5288212a082
AnakinAli <anakinali.personal@gmail.com>
2023-11-24 18:42:44 +0200

    fixed termapaper - 2.2

--- TermPaper-2-2-23.11/main.cpp
+++ TermPaper-2-2-23.11/main.cpp
@@ -14,19 +14,20 @@ T getInput(const string&prompt) {
 int main() {
     int rows = getInput<int>("Въведи n: ");
     int cols = getInput<int>("Въведи m: ");
-    double y[rows][cols];
+    double** y = new double *[rows];
 
-    double T[rows];
-    int c = 0;
+    double** T = new double *[rows];
+
 
     for (int row = 0; row < rows; row++) {
-        T[row] = 0;
+        T[row] = new double{0};
+        y[row] = new double[cols];
         for (int col = 0; col < cols; col++) {
             cout << endl << "---------" << "Ред: " << row + 1 << " Колона: " << col + 1 << "---------" << endl;
 
             y[row][col] = getInput<double>("Въведете стойност: ");
             if (y[row][col] < 0) {
-                T[row] += y[row][col];
+                T[row][0] += y[row][col];
             }
         }
     }
@@ -43,6 +44,16 @@ int main() {
 
 
     for (int row = 0; row < rows; row++) {
-        cout << "Ред №" << row + 1 << ":" << T[row] << endl;
+        cout << "Ред №_" << row + 1 << ": " << T[row][0] << endl;
     }
+
+
+    for (int i = 0; i < rows; i++) {
+        delete [] y[i];
+    }
+    delete y;
+    for (int i = 0; i < rows; i++) {
+        delete [] T[i];
+    }
+    delete T;
 }
